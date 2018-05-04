ca2185f7c029aebac24f6b12c7ba09367a227f23
GanYihuan <583520052@qq.com>
2018-05-05 07:58:27 +0800

    06-Template Strings

--- ECMAScript6新功能快速入门32课/06-Template Strings.js
+++ ECMAScript6新功能快速入门32课/06-Template Strings.js
@@ -5,8 +5,13 @@ let breafast = 'Today breakfast it is ' + dessert + ' and ' + drink + ' !';
 // es6
 let breafast2 = `Today breakfast it is \n ${dessert} and ${drink} !`;
 
+console.log(breafast2.startsWith('T'));
+console.log(breafast2.endsWith('!'));
+console.log(breafast2.includes('i'));
+
 // carry Tags template strings
 let breakfast3 = kitchen`Today breakfast it is ${dessert} and ${drink} !`;
+
 function kitchen(strings, ...values) {
   console.log(strings);
   console.log(values);
@@ -18,4 +23,5 @@ function kitchen(strings, ...values) {
   result += strings[strings.length - 1];
   return result;
 }
+
 console.log(breakfast3);
