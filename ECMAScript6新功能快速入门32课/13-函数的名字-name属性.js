c95e9bdb810ba3f1cc8036d68b9bab88922e0944
GanYihuan <583520052@qq.com>
2018-05-05 08:46:12 +0800

    13-函数的名字-name属性

---
+++ ECMAScript6新功能快速入门32课/13-函数的名字-name属性.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function breakfast() {
+}
+
+console.log(breakfast.name); // breakfast
+
+
+//
+let breakfast2 = function (args) {
+};
+console.log(breakfast2.name); // breakfast2
+
+
+//
+let breakfast3 = function superBreakfast(args) {
+};
+console.log(breakfast3.name); // superBreakfast
