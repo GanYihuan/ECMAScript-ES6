bba1b5cd8120d607f6f1fa30071a6c341a7160ee
GanYihuan <583520052@qq.com>
2018-06-09 11:06:05 +0800

    format 16

---
+++ web全栈之ECMAScript6/16-koa/server.js
@@ -0,0 +1,18 @@
+const koa = require('koa');
+const mysql = require('koa-mysql');
+
+let db = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '123456',
+  database: '20170702'
+});
+
+let server = new koa();
+
+// koa废弃了generator
+server.use(function* () {
+  let data = yield db.query(`SELECT * FROM user_table`);
+  this.body = data;
+});
+server.listen(8080);
